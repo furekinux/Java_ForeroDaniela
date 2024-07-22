c63503a009d3d9b3b6117d3036d625634ed40b14
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-07-22 13:38:02 -0500

    feat: :sparkles: Agregar menú

--- dia_01/CampusLandsERP/src/campuslandserp/CampusLandsERP.java
+++ dia_01/CampusLandsERP/src/campuslandserp/CampusLandsERP.java
@@ -17,38 +17,89 @@ public class CampusLandsERP {
      */
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        
         boolean start=true;
+        boolean coord=true;
+        boolean trainer=true;
+        boolean camper=true;
         
-        while (start=true){
-        System.out.println("-----Bienvenido-----\n¿Quién desea ingresar?\n  1. Coordinacion\n  2. Trainer\n  3. Camper");
-        int a = sc.nextInt();
-        /*nextInt recibe numeros int, nextLine recibe Strings*/
-        
-        switch (a) {
-        /*switch realiza revisión de los casos posibles(evitar usar el else if...)*/
-            case 1:
-                System.out.println("-----Coordinador-----\n  1.Asignación\n  2.Cerrar sesión");
-                int rta = sc.nextInt();
-                switch(rta){
-                    case 1:
-                        System.out.println("Asignación");
-                    case 2:
-                        System.out.println("Cerrando sesión");
-                        break;
-                    default: {
-                        System.out.println("No se reconoció el usuario");
-                        break;
+        while (start==true){
+            System.out.println("-----Bienvenido-----\n¿Quién desea ingresar?\n  1. Coordinacion\n  2. Trainer\n  3. Camper\n  4. Salir");
+            int a = sc.nextInt();
+            /*nextInt recibe numeros int, nextLine recibe Strings*/
+
+            switch (a) { /*switch realiza revisión de los casos posibles(evitar usar el else if...)*/
+
+                case 1 -> {
+                    while(coord==true){
+                        System.out.println("\n-----Coordinador-----\n  1. Asignación\n  2. Registrar\n  3. Cerrar sesión");
+                        int rta = sc.nextInt();
+                        switch(rta){
+                            case 1 ->
+                                System.out.println("Viste asignación\n");
+                            case 2 ->
+                                System.out.println("Viste Registrar\n");
+                            case 3 -> {
+                                System.out.println("Cerrando sesión\n");
+                                coord=false;
+                            }
+                            default ->
+                                System.out.println("No se reconoció la orden\n");
+                        }
                     }
                 }
-            case 2:
-                System.out.println("Hola Trainer");
-            case 3:
-                System.out.println("Hola Trainer");
-            default: {
-                System.out.println("No se reconoció el usuario");
+                
+                case 2 -> {
+                    while(trainer==true){
+                        System.out.println("\n-----Trainer-----\n  1. Ver listado\n  2. Ver salas\n  3. Cerrar sesión");
+                        int rta = sc.nextInt();
+                        switch (rta) {
+                            case 1 ->
+                                System.out.println("Viste listado\n");
+                            case 2 ->
+                                System.out.println("Viste salas\n");
+                            case 3 -> {
+                                System.out.println("Cerrando sesión\n");
+                                trainer=false;
+                            }
+                            default ->
+                                System.out.println("No se reconoció la orden\n");
+                        }
+                    }
+                }
+                
+                case 3 -> {
+                    while(camper==true){
+                        System.out.println("\n-----Camper-----\n  1. Ver notas\n  2. Ver curso\n  3. Cerrar sesión");
+                        int rta = sc.nextInt();
+                        switch (rta) {
+                            case 1 ->
+                                System.out.println("Viste notas\n");
+                            case 2 ->
+                                System.out.println("Viste curso\n");
+                            case 3 -> {
+                                System.out.println("Cerrando sesión\n");
+                                camper=false;
+                            }
+                            default ->
+                                System.out.println("No se reconoció la orden\n");
+                        }
+                    }
+                }
+                
+                case 4 -> {
+                    System.out.println("Cerrando sesión\n");
+                    start=false;
+                }
+                
+                default ->
+                    System.out.println("No se reconoció el usuario, vuelta a intentarlo\n");
             }
+            /*RESETEAR LOS CICLOS*/
+            coord=true;
+            trainer=true;
+            camper=true;
+// TODO code application logic here
         }
-        // TODO code application logic here
-            }
-        }
+    }
 }
--- dia_01/CampusLandsERP/src/campuslandserp/camper.java
+++ dia_01/CampusLandsERP/src/campuslandserp/camper.java
@@ -3,11 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package campuslandserp;
-
 /**
  *
  * @author camper
  */
 public class camper {
-
+   
 }
