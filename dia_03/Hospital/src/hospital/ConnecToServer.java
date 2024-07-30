fecc79220f3ef57766b53b0fd58b08567e145adb
Daniela Forero Ballen <156367697+furekinux@users.noreply.github.com>
2024-07-30 09:11:50 -0500

    fix: :wrench: Mover info de BBDD

--- dia_03/Hospital/src/hospital/ConnecToServer.java
+++ dia_03/Hospital/src/hospital/ConnecToServer.java
@@ -1,8 +1,11 @@
 package hospital;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  *
@@ -10,16 +13,24 @@ import java.sql.SQLException;
  */
 public class ConnecToServer {
     Connection con;
-
-    public ConnecToServer(){
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            /**/
-            String port="3306";
-            String direccion="bclzsd9hip6j1proyaic-mysql.services.clever-cloud.com";
-            String base_datos="bclzsd9hip6j1proyaic";
+    
+    public ConnecToServer() throws IOException{
+        Properties props = new Properties();
+        try(InputStream input = getClass().getClassLoader().getResourceAsStream("Config.properties")) {
             
-            con=DriverManager.getConnection("jdbc:mysql://"+direccion+":"+port+"/"+base_datos+"?useSSL=false","uzwx5o0vu4wtu0ti","OI8uB31cexc31UdLCabv");
+            if(input == null){
+                throw new IllegalStateException("Archivo no existente :(");
+            }
+            
+            props.load(input);
+            
+            String URL=props.getProperty("url");
+            String USER=props.getProperty("user");
+            String PSWRD=props.getProperty("password");
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            con=DriverManager.getConnection(URL,USER,PSWRD);
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println("Error:" +e);
         }
