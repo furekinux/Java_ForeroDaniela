c16ce086d65dfb6450e2e40e89f98682f189691c
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-08-21 14:20:00 -0500

    feat: :sparkles: Crear base de datos

---
+++ dia_16/BBDD/tables.sql
@@ -0,0 +1,19 @@
+use btw3qkrwrj0nqyquwu5w;
+
+create table tarjeta (
+	id_tarjeta int not null primary key auto_increment,
+    numero_cuenta varchar(20) not null,
+    valor_apertura decimal(10,2) not null,
+    mes enum("Enero",
+			"Febrero",
+			"Marzo",
+			"Abril",
+			"Mayo",
+			"Junio",
+			"Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre") not null
+);
