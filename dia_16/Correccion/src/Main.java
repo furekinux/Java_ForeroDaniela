cef866f6fca3f798ce0ce7e3c05e211ddbfc2b8a
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-08-21 14:19:29 -0500

    fix: :bandage: Modificar error input

--- dia_16/Correccion/src/Main.java
+++ dia_16/Correccion/src/Main.java
@@ -5,41 +5,6 @@ import persistencia.LecturaEscritura;
 public class Main {
     
     public static void main(String[] args) {
-        /*Cartera b1= new Cartera("2024");
-        b1.agregarTarjeta(new Joven("000-0005-001",20000,"Mayo"));
-        b1.agregarTarjeta(new Nomina("000-0005-002",110000,"Mayo")); 
-        b1.agregarTarjeta(new Visa("000-0005-003",90000,"Mayo")); 
-        b1.agregarTarjeta(new Joven("000-0005-004",22000,"Mayo")); 
-        b1.agregarTarjeta(new Joven("000-0006-005",35000,"Junio"));
-        b1.agregarTarjeta(new Nomina("000-0006-006",100000,"Junio")); 
-        b1.agregarTarjeta(new Visa("000-0006-007",85000,"Junio")); 
-        b1.agregarTarjeta(new Joven("000-0006-008",28000,"Junio")); 
-        b1.imprimirListaDeTarjetas();*/
-        /*LecturaEscritura le = new LecturaEscritura("Enero");
-        le.leerTarjetas();
-        le.setFileName("Febrero");
-        le.leerTarjetas();
-        le.setFileName("Marzo");
-        le.leerTarjetas();
-        le.setFileName("Abril");
-        le.leerTarjetas();
-        le.setFileName("Mayo");
-        le.leerTarjetas();
-        le.setFileName("Junio");
-        le.leerTarjetas();
-        le.setFileName("Julio");
-        le.leerTarjetas();
-        le.setFileName("Agosto");
-        le.leerTarjetas();
-        le.setFileName("Septiembre");
-        le.leerTarjetas();
-        le.setFileName("Octubre");
-        le.leerTarjetas();
-        le.setFileName("Noviembre");
-        le.leerTarjetas();
-        le.setFileName("Diciembre");
-        le.leerTarjetas();
-        le.escribirTarjetas(le.getList());*/
         int opcion = 3;
         do{
             System.out.println("""
@@ -81,7 +46,7 @@ public class Main {
                 }
                 case 2 -> {System.out.println("Digite el mes que desea consultar: ");
                             String mes = sc.next();
-                            consultarTarjetaMes(mes.toLowerCase());
+                            consultarTarjetaMes(mes.toUpperCase().charAt(0)+mes.substring(1, mes.length()));
                 }
                 default -> {System.out.println("Digite una opción correcta del Menú");
                 }
