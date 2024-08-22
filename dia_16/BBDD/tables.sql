51244ce8f5ad330b0a1619d51bb27f28edc275d6
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-08-22 07:03:05 -0500

    feat: :sparkles: Modificar atributo

--- dia_16/BBDD/tables.sql
+++ dia_16/BBDD/tables.sql
@@ -2,9 +2,9 @@ use btw3qkrwrj0nqyquwu5w;
 
 create table tarjeta (
 	id_tarjeta int not null primary key auto_increment,
-    numero_cuenta varchar(20) not null,
-    valor_apertura decimal(10,2) not null,
-    mes enum("Enero",
+	numero_cuenta varchar(20) not null,
+    	valor_apertura decimal(10,2) not null,
+    	mes enum("Enero",
 			"Febrero",
 			"Marzo",
 			"Abril",
@@ -16,4 +16,5 @@ create table tarjeta (
             "Octubre",
             "Noviembre",
             "Diciembre") not null
+	tipo enum("Joven","Nomina","Visa") not null
 );
