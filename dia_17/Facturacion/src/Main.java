868be01e29e23ab161232d5d108417ff922322ca
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-08-22 14:17:20 -0500

    feat: :sparkles: Agregar atributos a clases

--- dia_17/Facturacion/src/Main.java
+++ dia_17/Facturacion/src/Main.java
@@ -1,12 +1,29 @@
 
-import java.util.Scanner;
+import logica.Cliente;
+import logica.Factura;
+import logica.Producto;
+
+/*import java.util.Scanner;*/
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Cliente cliente1 = new Cliente("Juan", "Carlos", "999999999");
+        
+        Producto producto1 = new Producto("Un zapato(derecho)", 999.99);
+        Producto producto2 = new Producto("Un zapato(izquierdo)", 25.99);
+
+        Factura factura1 = new Factura(cliente1);
+        
+        factura1.agregarProducto(producto1);
+        factura1.agregarProducto(producto2);
+        
+        factura1.listarProducto();
+                
+        /*Scanner sc = new Scanner(System.in);
         boolean menu = true;
         int a;
+        
         while(menu){
             System.out.println("----- Menu Principal -----\n 1. Agregar productos a la factura\n 2. Calcular total\n 3. Aplicar descuentos\n 0. Salir");
             a = sc.nextInt();
@@ -20,7 +37,7 @@ public class Main {
                     menu=false;
                 }
             }
-        }
+        }*/
 
     }
     
--- dia_17/Facturacion/src/logica/Factura.java
+++ dia_17/Facturacion/src/logica/Factura.java
@@ -1,5 +1,73 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factura {
     
+    private Cliente cliente;
+    private List<Producto> productos;
+    private double total;
+    private double descuento;
+
+    public Factura(Cliente cliente) {
+        this.cliente = cliente;
+        this.productos = new ArrayList<>();
+        this.total = calcularTotal();
+    }
+
+    public Factura(Cliente cliente, List<Producto> productos) {
+        this.cliente = cliente;
+        this.productos = productos;
+        this.total = calcularTotal();
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+        this.total = calcularTotal();
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+    
+    private double calcularTotal(){
+        double a=0;
+        for (Producto producto : productos) {
+            a=a+producto.getPrecio();
+        }
+        return a;
+    }
+    
+    public void agregarProducto(Producto p){
+        productos.add(p);
+        this.total = calcularTotal();
+    }
+    
+    public void listarProducto(){
+        System.out.println("\nActualmente tiene "+productos.size()+" productos:");
+        for(Producto producto : productos){
+            System.out.println(producto.getNombre()+" "+producto.getPrecio());
+        }
+    };
 }
